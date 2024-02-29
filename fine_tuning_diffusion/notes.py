2c19aa0b54df78e5a498944070824eed967145e5
Danjie Tang <37476565+DanjieTang@users.noreply.github.com>
2024-02-29 14:12:15 -0500

    Update notes.py

--- fine_tuning_diffusion/notes.py
+++ fine_tuning_diffusion/notes.py
@@ -20,7 +20,7 @@ accelerate launch train_text_to_image_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --dataset_name=$DATASET_NAME --caption_column="text" \
   --resolution=512 --random_flip \
-  --train_batch_size=1 \
+  --train_batch_size=4 \
   --num_train_epochs=100 --checkpointing_steps=5000 \
   --learning_rate=1e-04 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --seed=42 \
