fd948a73dfbe6b26f2169d4aedcdfbab7c3500e6
Danjie Tang <37476565+DanjieTang@users.noreply.github.com>
2024-02-29 14:08:35 -0500

    Update notes.py

--- fine_tuning_diffusion/notes.py
+++ fine_tuning_diffusion/notes.py
@@ -24,5 +24,5 @@ accelerate launch train_text_to_image_lora.py \
   --num_train_epochs=100 --checkpointing_steps=5000 \
   --learning_rate=1e-04 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --seed=42 \
-#   --output_dir="sd-pokemon-model-lora" \
-#   --validation_prompt="cute dragon creature" --report_to="wandb"
+  --output_dir="sd-pokemon-model-lora" \
+  --validation_prompt="cute dragon creature" --report_to="wandb"
