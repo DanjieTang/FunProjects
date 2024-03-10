05f4182a0563deb55c128d6f56396b374c1d052f
Danjie <danjie.tang@outlook.com>
2024-03-10 00:55:20 -0500

    I am learning about docker -v.

--- docker/test.py
+++ docker/test.py
@@ -5,4 +5,6 @@ app = FastAPI()
 
 @app.get("/hello")
 def my_func() -> str:
+    with open("storage/hey.txt", 'w') as file:
+        file.write("Yo what's up")
     return "I love docker -- not, sike! loll. hahhaha. djdj"
