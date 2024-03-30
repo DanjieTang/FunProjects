d8d4448286c49366293b750a089cba5563dc8fd1
Danjie <danjie.tang@outlook.com>
2024-03-30 18:24:47 -0400

    Updating openai endpoint since previous one is no longer supported.

--- llm_endpoint/openai_llm.py
+++ llm_endpoint/openai_llm.py
@@ -1,27 +1,31 @@
-from llm_endpoint.llm import LLM
+from llm import LLM
 from dotenv import load_dotenv
 from os import environ
-import openai
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(api_key=environ["OPENAI_API_KEY"])
 
 class OpenAILLM(LLM):
     def __init__(self, model: str = "gpt-4-1106-preview"):
         load_dotenv()
-        openai.api_key = environ["OPENAI_API_KEY"]
         self.model = model
     def llm_response(self, user_message: str, system_message: str = "You are a helpful assistant.") -> str:
         """
         Wrapper for OpenAI chat completion API.
-        
+
         :param user_message: User message.
         :param system_message: System message.
         :return: The model message.
         """
-        response = openai.ChatCompletion.create(
-            model=self.model,  # or another model like "text-davinci-003"
-            messages=[
-                {"role": "system", "content": system_message},
-                {"role": "user", "content": user_message}
-            ]
-        )
+        response = client.chat.completions.create(model=self.model,  # or another model like "text-davinci-003"
+        messages=[
+            {"role": "system", "content": system_message},
+            {"role": "user", "content": user_message}
+        ])
 
-        return response['choices'][0]['message']['content']
+        return response.choices[0].message.content
+
+llm = OpenAILLM()
+print(llm.llm_response("Say something funny"))
