c6db0003da02c8152bef20a9435e8f66337e43de
Danjie <danjie.tang@outlook.com>
2024-04-13 00:41:05 -0400

    Update example.py

--- parallel_processing/example.py
+++ parallel_processing/example.py
@@ -1,12 +1,26 @@
 from multiprocessing import Pool
+import time
 
 def square_number(n):
-    return n * n
+    for i in range(1000000000):
+        number = i
 
 if __name__ == '__main__':
+    # Sequential
+    start_time = time.time()
+    for i in range(4):
+        square_number(i)
+    
+    end_time = time.time()
+    print(end_time-start_time)
+    
     # Create a pool of workers
+    start_time = time.time()
     with Pool(processes=4) as pool:
-        numbers = [1, 2, 3, 4, 5]
+        end_time = time.time()
+        print(end_time-start_time)
+        numbers = [1, 2, 3, 4]
         # Map 'square_number' to the inputs and collect the output
         results = pool.map(square_number, numbers)
-        print(results)
+    end_time = time.time()
+    print(end_time-start_time)
