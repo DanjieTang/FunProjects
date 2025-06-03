f7dfda88c0ff58391a11d3f5b69280ac44d9fdac
Danjie Tang <danjie.tang@outlook.com>
2025-06-04 00:40:40 +0800

    Oof, github doesn't work in china

--- typescript/test.js
+++
@@ -1,8 +0,0 @@
-var myFirstObject = {
-    "First Name": "Tony",
-    "Last Name": "Stark"
-};
-myFirstObject["First Name"] = "Danjie";
-myFirstObject["Last Name"] = "Tang";
-myFirstObject["Age"] = 22;
-console.log(myFirstObject["First Name"] + " " + myFirstObject["Last Name"] + " " + myFirstObject["Age"]);
