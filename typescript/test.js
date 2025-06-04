52d71281705c2c12155ca838b7a3c35736cede8f
Danjie Tang <danjie.tang@outlook.com>
2025-06-04 10:11:16 +0800

    Messing around with typescript

---
+++ typescript/test.js
@@ -0,0 +1,4 @@
+var number = 41;
+var mood = "Happy";
+var answer = number === 41 ? mood === "Happy" ? "Nice" : "Ok" : number === 41 ? "Alright" : "No sir";
+console.log(answer);
---
+++ typescript/test.ts
@@ -0,0 +1,6 @@
+let number: number = 41;
+let mood: string = "Happy";
+
+let answer: string = number === 41 ? mood === "Happy" ? "Nice" : "Ok" : number === 41 ? "Alright": "No sir";
+
+console.log(answer)
